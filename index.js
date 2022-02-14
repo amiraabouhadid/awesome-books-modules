d0991f5a92610f81cf48886c1d2a341e8049ae7e
amira <amiraabouhadid@gmail.com>
2022-02-14 18:00:11 +0200

    fix linter errors

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import UserInterface from './modules/UserInterface.mjs';
-import  updateDate from './modules/DateTime.mjs';
-
+import updateDate from './modules/DisplayDateTime.mjs';
 
 UserInterface.populateContainer();
-
 UserInterface.addButton.addEventListener('click', UserInterface.addBook);
-
-
 window.setInterval(updateDate, 1000);
